dd9652a9fcb9cb4b65b68848da6a1c2c2e5b986e
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-26 17:31:56 +0200

    extends

--- sat/musk.py
+++ sat/musk.py
@@ -54,7 +54,7 @@ def unit_propagation(formula, unit_clauses=None):
     while unit_clauses:
         unit = unit_clauses[0]
         formula = bcp(formula, unit[0])
-        assignment += [unit[0]]
+        assignment.extend(unit)
         if formula == -1 or not formula:
             return formula, assignment
         unit_clauses = [c for c in formula if len(c) == 1]
@@ -63,9 +63,9 @@ def unit_propagation(formula, unit_clauses=None):
 
 def solve(formula, assignment, unit=None):
     formula, unit_assignment = unit_propagation(formula, unit)
-    assignment += unit_assignment
     if formula == - 1:
         return []
+    assignment.extend(unit_assignment)
     if formula == []:
         return assignment
     variable = get_literal(formula)
@@ -79,7 +79,7 @@ def main():
 
     if solution:
         fill = lambda i: i if i not in solution and -i not in solution else None
-        solution += list(filter(lambda x: x is not None, map(fill, range(1, variables + 1))))
+        solution.extend(filter(lambda x: x is not None, map(fill, range(1, variables + 1))))
         # solution += [i for i in range(1, variables + 1) if i not in solution and -i not in solution]
         solution.sort(key=abs)
         print('s SATISFIABLE' + '\n' + 'v ' + ' '.join([str(x) for x in solution]) + ' 0')
